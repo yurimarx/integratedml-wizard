3c640454cd80868ab939b7f7690ebbbf71ebffee
Yuri Gomes <yurimarx@gmail.com>
2020-07-12 22:04:32 -0300

    include frontend to static spring boot page

---
+++ src/main/resources/static/precache-manifest.e14f4cfe609889b469480f6bd7e5b51c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a31364fa0f7983dfeb3bec98e9b7ec2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1670629b3c12fbc8df51",
+    "url": "./static/css/2.11f6f215.chunk.css"
+  },
+  {
+    "revision": "f47f5a03e94a146a0c2f",
+    "url": "./static/css/main.10390f2c.chunk.css"
+  },
+  {
+    "revision": "1670629b3c12fbc8df51",
+    "url": "./static/js/2.eb524b82.chunk.js"
+  },
+  {
+    "revision": "35b34e1a587b178bf9b8096b0e77020d",
+    "url": "./static/js/2.eb524b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47f5a03e94a146a0c2f",
+    "url": "./static/js/main.20b61231.chunk.js"
+  },
+  {
+    "revision": "f01124eb65adc8f72663",
+    "url": "./static/js/runtime-main.34c535d1.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "./static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "./static/media/line.567f5738.gif"
+  }
+]);
